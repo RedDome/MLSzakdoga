eb189d5f475fb6f5825513a7eb03a76d98066a35
RedDome <vorosdome580089@gmail.com>
2025-01-29 19:36:05 +0100

    runprogram expanded, works now

--- runprogram.py
+++ runprogram.py
@@ -1,18 +1,13 @@
 import subprocess
 import time
 
-# Path to your .sh file
 sh_file_path = './start_ros.sh'
+config_file_path = '/workspaces/MLSzakdoga/config/LEARN_DEFAULT_CONFIG.yaml'
 
-# Run the shell script in the background
-# subprocess.Popen(['bash', sh_file_path])
+subprocess.Popen(['bash', sh_file_path])
 
-# Wait a moment to ensure the shell script has started (optional, depending on your use case)
-# time.sleep(5)
+time.sleep(15)
 
 print("Sleep completed")
 
-# Call another Python script
-subprocess.run(['python3', 'src/main.py'])
-
-print("Shell script is running in the background, and the second Python script has been called.")
+subprocess.run(['python3', 'src/main.py', config_file_path])
